f3386f63b4907253de863f2883d7b99497fe1437
Karolina <krybarczyk@sigma.ug.edu.pl>
2011-01-18 09:43:49 +0100

    Zadanie 4

---
+++ zad4.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+echo 
+ls
+echo 
+echo zad1
+echo 
+sed 99q zad1
+echo 
+echo ==============================
+echo
+echo zad2
+echo
+sed 99q zad2   
+echo
+echo ==============================
+echo
+echo zad3
+echo
+sed 99q zad2
+echo
+echo ==============================
+echo
+echo zad4
+echo
+sed 99q zad2
+echo
+echo ==============================
+echo
+echo zad5
+echo
+sed 99q zad2
+echo
+echo ==============================
+echo
+echo zad6
+echo
+sed 99q zad2
+echo
+echo ==============================
+echo
+echo zad7
+echo
+sed 99q zad7
+echo
+echo ==============================
+echo
+echo zad8
+echo
+sed 99q zad8
+echo
+echo ==============================
+echo
+echo zad9
+echo
+sed 99q zad9
+echo
+echo ==============================
+echo
+echo zad10
+echo
+sed 99q zad10
+echo
+echo ==============================
+echo
+echo zad11
+echo
+sed 99q zad11
+echo
+echo ==============================
+echo
+echo zad12
+echo
+sed 99q zad12
+echo
+echo ==============================
+echo
+echo zad13
+echo
+sed 99q zad13
+echo
+echo ==============================
+echo
+echo zad14
+echo
+sed 99q zad14
+echo
+echo ==============================
+echo
+echo zad15
+echo
+sed 99q zad15
+echo
+echo ==============================
+echo
+echo zad16
+echo
+sed 99q zad16
+echo
+echo ==============================
+echo
+echo zad17
+echo
+sed 99q zad17
+echo
+echo ==============================
+echo
+echo zad18
+echo
+sed 99q zad18
+echo
+echo ==============================
+echo
+echo zad19
+echo
+sed 99q zad19
+echo
+echo ==============================
+echo
+echo zad20
+echo
+sed 99q zad20
+echo
+echo ==============================
+echo
+echo zad21
+echo
+sed 99q zad22
+echo
+echo ==============================
+echo
+echo zad22
+echo
+sed 99q zad22
+echo
+echo ==============================
+echo
+echo zad23
+echo
+sed 99q zad23
+echo
+echo ==============================
+echo
+echo zad24
+echo
+sed 99q zad24
+echo
+echo ==============================
+echo
+echo zad25
+echo
+sed 99q zad25
+echo
+echo ==============================
+
